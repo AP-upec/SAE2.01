5619b006209ca838d3ee151fd62cbfd99a9854ec
AP-upec <aaron.piscitello@etu.u-pec.fr>
2026-06-09 11:29:23 +0200

    retrait_page

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from config import Config
 from controllers.accueil import bp_accueil
 from controllers.api import bp_api
 from controllers.effectifs import bp_effectifs
-from controllers.page import bp_page
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -13,7 +12,6 @@ app.config.from_object(Config)
 app.register_blueprint(bp_accueil)
 app.register_blueprint(bp_api)
 app.register_blueprint(bp_effectifs)
-app.register_blueprint(bp_page)
 
 # Gestion des erreurs
 @app.errorhandler(404)
