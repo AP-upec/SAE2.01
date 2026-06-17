c8c89483a56a196bad0adb4bb64d3c8456e048d7
Zeffut <tom77ds@gmail.com>
2026-06-17 14:26:39 +0200

    carte_densites_par_profession

--- controllers/dentistes.py
+++ controllers/dentistes.py
@@ -1,31 +1,45 @@
-from flask import Blueprint, render_template
-from models.dimensions import Departement
+from flask import Blueprint, render_template, request, jsonify
+from models.dimensions import ProfessionSante
 from models.db import Session
 from services.ameli_api import AmeliAPI
 
 bp = Blueprint("dentistes", __name__, url_prefix="/dentistes")
+api = AmeliAPI()
 
-# Profession et année utilisées pour la carte de densité
-PROFESSION_DENTISTE = "Ensemble des chirurgiens-dentistes"
-ANNEE = 2023
+# Années disponibles dans le dataset ameli (de la plus récente à la plus ancienne)
+ANNEES = list(range(2024, 2009, -1))
 
 
 @bp.route("/")
 def carte_dentistes():
-    api = AmeliAPI()
+    """Carte interactive des densités de professionnels de santé par département."""
     session = Session()
     try:
-        # Un seul appel groupé (tous les départements) au lieu d'un appel par département
-        effectifs = api.get_effectifs_par_departement(PROFESSION_DENTISTE, ANNEE)
-        donnees = []
-        for dept in session.query(Departement).order_by(Departement.code).all():
-            infos = effectifs.get(dept.code, {})
-            donnees.append({
-                "code": dept.code,
-                "libelle": dept.libelle,
-                "effectif": infos.get("effectif") or 0,
-                "densite": infos.get("densite") or 0,
-            })
+        professions = (session.query(ProfessionSante)
+                       .order_by(ProfessionSante.libelle).all())
     finally:
         session.close()
-    return render_template("carte_dentistes.html", donnees=donnees, annee=ANNEE)
+    return render_template("carte_dentistes.html",
+                           professions=professions, annees=ANNEES)
+
+
+@bp.route("/densites")
+def densites():
+    """Densité et effectif de chaque département pour une profession et une année.
+    Renvoyé en JSON et consommé par la carte en AJAX (sans recharger la page)."""
+    profession_id = request.args.get("profession_id", type=int)
+    annee = request.args.get("annee", type=int) or 2023
+    session = Session()
+    try:
+        prof = session.get(ProfessionSante, profession_id) if profession_id else None
+        if prof is None:
+            prof = (session.query(ProfessionSante)
+                    .order_by(ProfessionSante.libelle).first())
+        departements = api.get_effectifs_par_departement(prof.libelle, annee)
+    finally:
+        session.close()
+    return jsonify({
+        "profession": prof.libelle,
+        "annee": annee,
+        "departements": departements,
+    })
